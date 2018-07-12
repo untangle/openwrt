87db1b4965fa0a5fe333687eb6a18a1659e493a2
Thibaut VARÈNE <hacks@slashdirt.org>
2018-07-12 13:43:36 +0200

    ar71xx: add support for MikroTik RB931-2nD
    This patch adds support for the MikroTik RB931-2nD (hAP mini):
    https://mikrotik.com/product/RB931-2nD
    
    Specifications:
      * SoC: Qualcomm QCA9533 (650MHz)
      * RAM: 32MiB
      * Storage: 16MiB SPI NOR flash
      * Ethernet: 3x100M
      * Wireless: QCA9533 built-in, dual-chain 802.11b/g/n
    
    Installation:
    
    1. Setup a DHCP/BOOTP Server with the following parameters:
       * DHCP-Option 66 (TFTP server name): pointing to a local TFTP
         server within the same subnet of the DHCP range
       * DHCP-Option 67 (Bootfile-Name): matching the initramfs filename
         of the to be booted image. The usable intramfs files are:
           - openwrt-ar71xx-mikrotik-vmlinux-initramfs.elf
           - openwrt-ar71xx-mikrotik-vmlinux-initramfs-lzma.elf
           - openwrt-ar71xx-mikrotik-rb-nor-flash-16M-initramfs-kernel.bin
    
    2. Press the reset button on the board and keep that pressed.
    
    3. Connect the board to your local network via its Internet port.
    
    4. Release the button after the LEDs on the board are turned off.
       Now the board should load and start the initramfs image from
       the TFTP server.
    
    5. Now connect the board via either of its LAN ports (2 or 3).
    
    6. Upload the sysupgrade image to the board with scp:
         $ scp openwrt-ar71xx-mikrotik-rb-nor-flash-16M-squashfs-sysupgrade.bin root@192.168.1.1:/tmp/fw.bin
    
    7. Log in to the running system listening on 192.168.1.1 via ssh
       as root (without password):
         $ ssh root@192.168.1.1
    
    8. Flash the uploaded firmware file from the ssh session via the
       sysupgrade command:
         root@OpenWrt:~# sysupgrade /tmp/fw.bin
    
    Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -707,6 +707,7 @@ platform_check_image() {
 	rb-750up-r2|\
 	rb-911-2hn|\
 	rb-911-5hn|\
+	rb-931-2nd|\
 	rb-941-2nd|\
 	rb-951ui-2nd|\
 	rb-952ui-5ac2nd|\
@@ -734,6 +735,7 @@ platform_pre_upgrade() {
 	rb-750up-r2|\
 	rb-911-2hn|\
 	rb-911-5hn|\
+	rb-931-2nd|\
 	rb-941-2nd|\
 	rb-951ui-2nd|\
 	rb-952ui-5ac2nd|\
