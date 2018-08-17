34960fad88c64d5459b0b88eb570ba665535b5b7
Sébastien Delafond <sdelafond@gmail.com>
2018-08-17 11:11:09 +0200

    Change semantics of build.sh's -v

--- build.sh
+++ build.sh
@@ -4,9 +4,14 @@ set -e
 set -x
 
 usage() {
-  echo "Usage: $0 [-d <device>] [-l <libc>]"
-  echo "  -d <device> : x86_64, wrt3200, ... (defaults to x86_64)"
-  echo "  -l <libc>   : musl, glibc (defaults to musl)"
+  echo "Usage: $0 [-d <device>] [-l <libc>] [-v (latest|<branch>|<tag>)]"
+  echo "  -d <device>              : x86_64, wrt3200, wrt1900 (defaults to x86_64)"
+  echo "  -l <libc>                : musl, glibc (defaults to musl)"
+  echo "  -v latest|<branch>|<tag> : version to build from (defaults to master)"
+  echo "                             - 'release' is a special keyword meaning 'most recent tag from each"
+  echo "                                package's source repository'"
+  echo "                             - <branch> or <tag> can be any valid git object as long as it exists"
+  echo "                               in each package's source repository (mfw_admin, packetd, ngfw_pkgs, etc)"
   exit 1
 }
 
