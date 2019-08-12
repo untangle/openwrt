3479fe379baba717de532d1b99728dd08ad701a1
Florian Eckert <fe@dev.tdt.de>
2019-08-12 12:04:04 +0200

    target/x86/image: use qemu-img from STAGING_DIR_HOST
    The host tool package qemu is build for the target x86. The installed tool
    qemu-img is needed to build vdi or vmdk images. In the image Makefile we
    use however the host installed qemu-img command and not the tool from the
    buildsystem. This commit force to use the already build qemu-img command
    from the openwrt toolchain.
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -291,7 +291,7 @@ endef
 # Convert a raw image into a $1 type image.
 # E.g. | qemu-image vdi
 define Build/qemu-image
-	qemu-img convert -f raw -O $1 $@ $@.new
+	$(STAGING_DIR_HOST)/bin/qemu-img convert -f raw -O $1 $@ $@.new
 	@mv $@.new $@
 endef
 
